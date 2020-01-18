11b55d65fdff00ac0c5ee56259b955f3a1519d58
Jonathan Tsai <jontsai@easypost.com>
2020-01-18 11:46:43 -0800

    adding files from react-native-web template

---
+++ build/precache-manifest.ed14a20798dda5a4312b32f88449a1cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8daefe9593d9d9ada5a4ba88c663dea8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c41a44cd0d3b7589a28c",
+    "url": "/static/js/2.d70a4cee.chunk.js"
+  },
+  {
+    "revision": "5b290a3fbb71598f98b4e825752db0f7",
+    "url": "/static/js/2.d70a4cee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "746cdc441a73b22dfcf2",
+    "url": "/static/js/main.eda3a4ae.chunk.js"
+  },
+  {
+    "revision": "ddae76cf60b472bde7e6",
+    "url": "/static/js/runtime-main.9f522239.js"
+  }
+]);
