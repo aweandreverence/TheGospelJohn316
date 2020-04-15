de4d33535077b6fc16051fb5587b7a0d1607b14c
Elijah Ang <eang0521@users.noreply.github.com>
2020-04-15 05:33:21 +0000

    Added a few frames to the app

--- src/storyboard/elements.js
+++ src/storyboard/elements.js
@@ -34,15 +34,10 @@ const StoryboardElements = {
         </StoryboardElement>
     ),
     'lineToLife': (
-        <StoryboardElement
-           style={{
-                fontSize: 18,
-                fontWeight: 'bold'
-           }}
-        >
+        <StoryboardElement>
           <div
             style={{
-                width: 230,
+                width: 280,
                 height: 3,
                 backgroundColor: '#000'
             }}
@@ -58,6 +53,170 @@ const StoryboardElements = {
         >
         LIFE
         </StoryboardElement>
+    ),
+    'redX': (
+	    <StoryboardElement
+	style={{
+	    fontSize: 50,
+	    fontWeight: 'bold',
+	    color: '#FF0000'
+	}}
+	    >
+	    x
+	</StoryboardElement>
+    ),
+    'lineToDeath': (
+	    <StoryboardElement>
+	    <div
+	style={{
+	    width: 310,
+	    height: 3,
+	    backgroundColor: '#000'
+	}}
+	    ></div>
+	    </StoryboardElement>
+    ),
+    'death': (
+	    <StoryboardElement
+	style={{
+	    fontSize: 18,
+	    fontWeight: 'bold'
+	}}
+	    >
+	    DEATH
+	</StoryboardElement>
+    ),
+    'sin': (
+	    <StoryboardElement
+	style={{
+	    fontSize: 18,
+	    fontWeight: 'bold',
+	    color: '#FF0000'
+	}}
+	    >
+	    SIN
+	</StoryboardElement>
+    ),
+    'treeTrunk': (
+	    <StoryboardElement>
+	    <div
+	style={{
+	    width: 20,
+	    height: 75,
+	    backgroundColor: '#643200'
+	}}
+	    ></div>
+	    </StoryboardElement>
+    ),
+    'treeLeaves': (
+	    <StoryboardElement>
+	    <div
+	style={{
+	    width: 80,
+	    height: 75,
+	    backgroundColor: '#00B800'
+	}}
+	    ></div>
+	    </StoryboardElement>
+    ),
+    'sins': (
+	    <StoryboardElement
+	style={{
+	    fontSize: 18,
+	    fontWeight: 'bold',
+	    color: '#FFB68A'
+	}}
+	    >
+	    SINS
+	</StoryboardElement>
+    ),
+    'chairBack': (
+	    <StoryboardElement>
+	    <div
+	style={{
+	    width: 5,
+	    height: 50,
+	    backgroundColor: '#000'
+	}}
+	    ></div>
+	    </StoryboardElement>
+    ),
+    'chairSeat': (
+	    <StoryboardElement>
+	    <div
+	style={{
+	    width: 40,
+	    height: 5,
+	    backgroundColor: '#000'
+	}}
+	    ></div>
+	    </StoryboardElement>
+    ),
+    'chairLeg': (
+	    <StoryboardElement>
+	    <div
+	style={{
+	    width: 3,
+	    height: 40,
+	    backgroundColor: '#000'
+	}}
+	    ></div>
+	    </StoryboardElement>
+    ),
+    's': (
+	    <StoryboardElement
+	style={{
+	    fontSize: 18,
+	    fontWeight: 'bold',
+	    color: '#FF0000'
+	}}
+	    >
+	    S
+	</StoryboardElement>
+    ),
+    'bigI': (
+	    <StoryboardElement
+	style={{
+	    fontSize: 60,
+	    fontWeight: 'bold',
+	    color: '#FF0000'
+	}}
+	    >
+	    I
+	</StoryboardElement>
+    ),
+    'n': (
+	    <StoryboardElement
+	style={{
+	    fontSize: 18,
+	    fontWeight: 'bold',
+	    color: '#FF0000'
+	}}
+	    >
+	    N
+	</StoryboardElement>
+    ),
+    'lineToGoodWorks': (
+	    <StoryboardElement>
+	    <div
+	style={{
+	    width: 3,
+	    height: 60,
+	    backgroundColor: '#000'
+	}}
+	    ></div>
+	    </StoryboardElement>
+    ),
+    'goodWorks': (
+	    <StoryboardElement
+	style={{
+	    fontSize: 18,
+	    fontWeight: 'bold',
+	    color: '#000'
+	}}
+	    >
+	    Good<br />Works
+	</StoryboardElement>
     )
 };
 
--- src/storyboard/frames.js
+++ src/storyboard/frames.js
@@ -21,14 +21,7 @@ const StoryboardFrames = {
                     x: 70,
                     y: 30,
                     z: 1
-                }
-            ]
-        }
-    },
-    2: {
-        layout: {
-            foundation: 1,
-            add: [
+                },
                 {
                     key: 'man',
                     x: 80,
@@ -38,9 +31,9 @@ const StoryboardFrames = {
             ]
         }
     },
-    3: {
+    2: {
         layout: {
-            foundation: 2,
+            foundation: 1,
             add: [
                 {
                     key: 'lineToLife',
@@ -50,12 +43,155 @@ const StoryboardFrames = {
                 },
                 {
                     key: 'life',
-                    x: 380,
+                    x: 430,
                     y: 30,
                     z: 1
                 }
             ]
         }
+    },
+    3: {
+	layout: {
+	    foundation: 2,
+	    add: [
+		{
+		    key: 'redX',
+		    x: 90,
+		    y: 2,
+		    z: 2
+		},
+		{
+		    key: 'man',
+		    x: 30,
+		    y: 200,
+		    z: 1
+		}
+	    ]
+	}
+    },
+    4: {
+	layout: {
+	    foundation: 3,
+	    add: [
+		{
+		    key: 'lineToDeath',
+		    x: 84,
+		    y: 212,
+		    z: 1
+		},
+		{
+		    key: 'death',
+		    x: 406,
+		    y: 200,
+		    z: 1
+		}
+	    ]
+	}
+    },
+    5: {
+	layout: {
+	    foundation: 4,
+	    add: [
+		{
+		    key: 'sin',
+		    x: 285,
+		    y: 85,
+		    z: 1
+		}
+	    ]
+	}
+    },
+    6: {
+	layout: {
+	    foundation: null,
+	    add: [
+		{
+		    key: 'treeTrunk',
+		    x: 100,
+		    y: 125,
+		    z: 1
+		},
+		{
+		    key: 'treeLeaves',
+		    x: 70,
+		    y: 50,
+		    z: 2
+		},
+		{
+		    key: 'sin',
+		    x: 95,
+		    y: 215,
+		    z: 1
+		},
+		{
+		    key: 'sins',
+		    x: 130,
+		    y: 15,
+		    z: 1
+		},
+		{
+		    key: 'chairBack',
+		    x: 305,
+		    y: 100,
+		    z: 1
+		},
+		{
+		    key: 'chairSeat',
+		    x: 305,
+		    y: 150,
+		    z: 1
+		},
+		{
+		    key: 'chairLeg',
+		    x: 312,
+		    y: 150,
+		    z: 1
+		},
+		{
+		    key: 'chairLeg',
+		    x: 335,
+		    y: 150,
+		    z: 1
+		},
+		{
+		    key: 's',
+		    x: 292,
+		    y: 125,
+		    z: 1
+		},
+		{
+		    key: 'bigI',
+		    x: 317,
+		    y: 80,
+		    z: 1
+		},
+		{
+		    key: 'n',
+		    x: 350,
+		    y: 125,
+		    z: 1
+		}
+	    ]
+	}
+    },
+    7: {
+	layout: {
+	    foundation: 5,
+	    add: [
+		{
+		    key: 'lineToGoodWorks',
+		    x: 100,
+		    y: 155,
+		    z: 1
+		},
+		{
+		    key: 'goodWorks',
+		    x: 75,
+		    y: 100,
+		    z: 1
+		}
+	    ]
+	}
     }
 
 };
